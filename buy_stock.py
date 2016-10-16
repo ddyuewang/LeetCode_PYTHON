757c75008a639977055e95bd16b68ad69f1691b5
yuewang <ywang@arc-investments.com>
2016-10-16 16:22:00 -0400

    add stock buy and sell

---
+++ buy_stock.py
@@ -0,0 +1,70 @@
+## buy stock version 1 - at most once
+class Solution_1(object):
+    def maxProfit(self, prices):
+        """
+        :type prices: List[int]
+        :rtype: int
+        """
+        if len(prices) < 2:
+            return 0
+        else:
+            profit = 0
+            curr_min = prices[0]
+            for i in range(0,len(prices)):
+                profit = max(profit, prices[i] - curr_min)
+                curr_min = min(curr_min, prices[i])
+
+            return profit
+
+## BUY stock version 2 - as many as you want
+
+class Solution_2(object):
+    def maxProfit(self, prices):
+        """
+        :type prices: List[int]
+        :rtype: int
+        """
+        if len(prices) < 2:
+            return 0
+        else:
+            profit = 0
+            for i in range(1,len(prices)):
+                profit +=  prices[i] - prices[i-1] if prices[i] - prices[i-1] >0 else 0
+            return profit
+
+## BUY stock version 3 - at most 2 transactions
+
+class Solution_3(object):
+    def maxProfit(self, prices):
+        """
+        :type prices: List[int]
+        :rtype: int
+
+        key: to construct two array f[i], g[j]
+             f[i] - profit for [0....i] days
+             g[j] - profit for [i+1....n-1] days
+        """
+        n = len(prices)
+        if n < 2:
+            return 0
+        else:
+            f = [0] * n
+            g = [0] * n
+            f_min = prices[0]
+            g_max = prices[-1]
+            f_profit = g_profit = 0
+            max_profit = 0
+
+            for i in range(0, n):
+                f_min = min(f_min, prices[i]) # here the order doesnt really matter, as if its min, profit will be unchanged
+                f_profit = max(f_profit, prices[i] - f_min)
+                f[i] = f_profit
+
+            for j in range(n-1,0,-1):
+                g_max = max(g_max, prices[j])
+                g_profit = max(g_profit, g_max - prices[j])
+                g[j] = g_profit
+
+            for i in range(0, n):
+                max_profit = max(max_profit, f[i] + g[i])
+            return max_profit
